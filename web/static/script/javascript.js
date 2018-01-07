d2a7cb6d72535f74c7dda65382a9e71969ed6b05
tom <tomf@vt.edu>
2018-01-07 08:48:30 -0500

    Back to ajax

--- web/static/script/javascript.js
+++ web/static/script/javascript.js
@@ -10,30 +10,17 @@ document.getElementById("submitButton").onclick = function(){
 		"recreation" : parseFloat(document.getElementById('recreation').value)
 	};
 
-	// $.ajax({
-	//     url: API_ENDPOINT,
-	//     type: 'POST',
-	//     data:  JSON.stringify(inputData),
-	// 	contentType: 'application/json; charset=utf-8',
-	//     success: function (response) {
-	// 		console.log(response);
-	//     },
-	//     error: function (xhr, exception) {
-	// 		console.log(xhr);
-	// 		console.log("error");
-	// 	}
-	// });
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", API_ENDPOINT, true);
-	xhr.setRequestHeader("Content-type", "application/json");
-	xhr.onreadystatechange = function () {
-	    if (xhr.readyState === 4 && xhr.status === 200) {
-	        var json = JSON.parse(xhr.responseText);
-	        console.log(json);
-	    }
-	};
-	var data = JSON.stringify(inputData);
-	console.log(data);
-	xhr.send(data);
+	$.ajax({
+	    url: API_ENDPOINT,
+	    type: 'POST',
+	    data:  JSON.stringify(inputData),
+		contentType: 'application/json; charset=utf-8',
+	    success: function (response) {
+			console.log(response);
+	    },
+	    error: function (xhr, exception) {
+			console.log(xhr);
+			console.log("error");
+		}
+	});
 }
