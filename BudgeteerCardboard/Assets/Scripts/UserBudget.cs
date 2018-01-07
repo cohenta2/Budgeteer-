d1d6cd5158af977edcc2c0b70686fa22d752e57f
Corey Sutphin <sutphincj@vcu.edu>
2018-01-06 23:26:39 -0500

    Initialized Cardboard VR App

---
+++ BudgeteerCardboard/Assets/Scripts/UserBudget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    public class UserBudget
+    {
+        public int food;
+        public int recreation;
+        public int rent;
+        public string username;
+        public int transportation;
+        public int income;
+    }
+}
---
+++ BudgeteerCardboard/Assets/Scripts/visualizeMoney.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+
+    // Handles visualization/animation of the money stacks based on a user's budget
+    public class NewBehaviourScript : MonoBehaviour
+    {
+
+        private UserBudget userBudget;
+
+        // Use this for initialization before app runs
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
